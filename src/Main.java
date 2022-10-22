d73def2cef36ac7e96746d38857b096a8e6a156e
Valeriy Torgovanov <Faust_XVI@list.ru>
2022-10-22 14:08:14 +0300

    v 1.0

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        String message = "";
+        String inputFileName = "openText.txt";
+        BufferedReader reader = new BufferedReader(new FileReader(new File(inputFileName)));
+        message = reader.readLine();
+        reader.close();
+        System.out.println(message);
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter your message:");
+//        message = scanner.nextLine();
+        // crete open and private keys
+        myRSA.getKey();
+        BigInteger messageCod = new BigInteger(message.getBytes());
+        // encrypt message
+        BigInteger encryptMessage = myRSA.encrypt(messageCod);
+        System.out.println("Encrypt message: " + encryptMessage);
+        // encrypt text writes in file
+        BufferedWriter writer = new BufferedWriter(new FileWriter(new File("encryptText.txt")));
+        writer.write(String.valueOf(encryptMessage));
+        writer.close();
+        BufferedReader newReader = new BufferedReader(new FileReader(new File("encryptText.txt")));
+        String encryptMessageFromFile = newReader.readLine();
+        newReader.close();
+        // check encrypt message
+        System.out.println(" Encrypt: " + encryptMessageFromFile);
+        BigInteger encryptMessage2 = new BigInteger (encryptMessageFromFile);
+        // decrypt message
+        BigInteger decryptMessage = myRSA.decrypt(encryptMessage2);
+        System.out.println("Decrypt message: " + new String(decryptMessage.toByteArray()));
+        BufferedWriter decrypt = new BufferedWriter(new FileWriter(new File("decryptText.txt")));
+        decrypt.write(new String(decryptMessage.toByteArray()));
+        decrypt.close();
+    }
+}
---
+++ src/myRSA.java
@@ -0,0 +1,69 @@
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+public class myRSA {
+    private final static BigInteger one = new BigInteger("1");
+    private static BigInteger privateKey;
+    private static BigInteger publicKey;
+    private static BigInteger n;
+    private static BigInteger q;
+    private static BigInteger p;
+    private static BigInteger eiler;
+    private static BigInteger e;
+    private static BigInteger d;
+
+
+    static void getKey()
+    {
+        p = BigInteger.probablePrime(1024, new SecureRandom());
+        System.out.println("p: " + p);
+        q = BigInteger.probablePrime(1024, new SecureRandom());
+        System.out.println("q: " + q);
+        n = p.multiply(q);
+        System.out.println("modulus (n): " + n);
+        eiler = (p.subtract(BigInteger.ONE)).multiply(q.subtract(BigInteger.ONE));
+        System.out.println("eiler: " + eiler);
+        e = new BigInteger("65537"); // common value in practice = 2^16 + 1
+        System.out.println("e: " + e);
+        d = e.modInverse(eiler);
+        publicKey = e;
+        System.out.println("publicKey: " + publicKey);
+        privateKey = d; //publicKey.modInverse(eiler);
+        System.out.println("privateKey: " + privateKey);
+    }
+
+    private static BigInteger findD(BigInteger e, BigInteger modulus)
+    {
+            BigInteger d;
+            for (d = new BigInteger("3"); !d.equals(modulus) ; d = d.add(BigInteger.ONE)) {
+                if (d.multiply(e).mod(modulus).equals(BigInteger.ONE)) {
+                    return d;
+                }
+            }
+            return d;
+    }
+    // (e*d) mod ((p-1)*(q-1))=1
+    private static BigInteger findE(BigInteger d, BigInteger modulus)
+    {
+        BigInteger e = modulus.subtract(BigInteger.TEN);
+        while (true)
+        {
+            if (((d.multiply(e)).mod(modulus)).equals(BigInteger.ONE))
+            {
+                return e;
+            }
+            else
+                e = e.subtract(BigInteger.ONE);
+        }
+        //return e;
+    }
+
+
+    static BigInteger encrypt(BigInteger message) {
+        return message.modPow(publicKey, n);
+    }
+
+    static BigInteger decrypt(BigInteger encrypted) {
+        return encrypted.modPow(privateKey, n);
+    }
+}
